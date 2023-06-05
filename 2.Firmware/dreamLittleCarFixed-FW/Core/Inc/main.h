d84e0fb04e76efa4b646e5f02684b3c262eb92a2
Fir-Geek <fir@live.it>
2023-06-05 15:27:09 +0800

    initial commit

---
+++ 2.Firmware/dreamLittleCarFixed-FW/Core/Inc/main.h
@@ -0,0 +1,101 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+static uint16_t tickCount = 0;
+extern uint8_t tickFlag;  //per 100ms turn 1
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RGB1_Pin GPIO_PIN_0
+#define RGB1_GPIO_Port GPIOA
+#define RGB2_Pin GPIO_PIN_1
+#define RGB2_GPIO_Port GPIOA
+#define RGB3_Pin GPIO_PIN_2
+#define RGB3_GPIO_Port GPIOA
+#define MOTOR2_CTRL1_Pin GPIO_PIN_6
+#define MOTOR2_CTRL1_GPIO_Port GPIOA
+#define MOTOR2_CTRL2_Pin GPIO_PIN_7
+#define MOTOR2_CTRL2_GPIO_Port GPIOA
+#define MOTOR1_PWM_Pin GPIO_PIN_0
+#define MOTOR1_PWM_GPIO_Port GPIOB
+#define MOTOR2_PWM_Pin GPIO_PIN_1
+#define MOTOR2_PWM_GPIO_Port GPIOB
+#define TRACK_OUT1_Pin GPIO_PIN_10
+#define TRACK_OUT1_GPIO_Port GPIOB
+#define TRACK_OUT2_Pin GPIO_PIN_11
+#define TRACK_OUT2_GPIO_Port GPIOB
+#define TRACK_OUT3_Pin GPIO_PIN_12
+#define TRACK_OUT3_GPIO_Port GPIOB
+#define TRACK_OUT4_Pin GPIO_PIN_13
+#define TRACK_OUT4_GPIO_Port GPIOB
+#define TRACK_OUT5_Pin GPIO_PIN_14
+#define TRACK_OUT5_GPIO_Port GPIOB
+#define MOTOR1_CTRL1_Pin GPIO_PIN_15
+#define MOTOR1_CTRL1_GPIO_Port GPIOB
+#define MOTOR1_CTRL2_Pin GPIO_PIN_8
+#define MOTOR1_CTRL2_GPIO_Port GPIOA
+#define HC05_RXD_Pin GPIO_PIN_9
+#define HC05_RXD_GPIO_Port GPIOA
+#define HC05_TXD_Pin GPIO_PIN_10
+#define HC05_TXD_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ 2.Firmware/dreamLittleCarFixed-FW/cmake-build-debug/CMakeFiles/dreamLittleCarFixed-FW.elf.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/startup/startup_stm32f103xb.s" "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/cmake-build-debug/CMakeFiles/dreamLittleCarFixed-FW.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Inc"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/gpio.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/main.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/stm32f1xx_it.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/syscalls.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/system_stm32f1xx.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/tim.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/tim.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Core/Src/usart.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "D:/Projects/dreamLittleCarFixed/2.Firmware/dreamLittleCarFixed-FW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj" "gcc" "CMakeFiles/dreamLittleCarFixed-FW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
